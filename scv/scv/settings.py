7404451b06e6099b387b1cc79bb2d60e0f05e282
htkim <htkim298@gmail.com>
2019-03-25 12:26:01 +0900

    Settings.py MEDIA_URL 항목 추가

--- scv/scv/settings.py
+++ scv/scv/settings.py
@@ -119,8 +119,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+AUTH_USER_MODEL = 'auth.User'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
